de042378b088a1d541af748bc90844156206089f
Colton Dowling <Colton@cd3style.com>
2017-08-01 15:59:09 -0500

    after making loop, Shannon helped me append some children to specific sections

--- starter_files/main.js
+++ starter_files/main.js
@@ -1,21 +1,79 @@
+//
+// console.log(customers);
+// console.log(cust[0]);
+// console.log(firstName);
+// console.log(fullName);
+// console.log(email);
+// console.log(StreetAddress);
+// console.log(CityStateZipAddress);
+// console.log(PhonyNumb);
 
-console.log(customers);
+// const cust = customers.results;
+// const firstName = cust[0].name.first;
+// const lastName = cust[0].name.last;
+// const fullName = `${firstName} ${lastName}`;
+// const email = cust[0].email
+// const StreetAddress = cust[0].location.street
+// const CityStateZipAddress =`${cust[0].location.city}, ${cust[0].location.state} ${cust[0].location.postcode} `;
+// const PhonyNumb = cust[0].phone;
+
+
+
+
+
+
+
+
+
 const cust = customers.results;
-console.log(cust[0]);
-const firstName = cust[0].name.first;
-console.log(firstName);
-const lastName = cust[0].name.last;
+// console.log('!!!! ' + cust.length);
+for (var i = 0; i < cust.length; i++) {
 
-const fullName = `${firstName} ${lastName}`;
-console.log(fullName);
+  // console.log('in for loop ' + i);
 
-const email = cust[0].email
-console.log(email);
+    const pictureURL = cust[i].picture.large;
+    const firstName = cust[i].name.first;
+    const lastName = cust[i].name.last;
+    const fullName = `${firstName} ${lastName}`;
+    const email = cust[i].email
+    const StreetAddress = cust[i].location.street
+    const CityStateZipAddress = `${cust[i].location.city}, ${cust[i].location.state} ${cust[i].location.postcode} `;
+    const PhonyNumb = cust[i].phone;
+    let PersonINFO = `
+ <img src='${pictureURL}'>
+ <h2>${fullName}</h2>
+ <p>${email}</p>
+ <p>${StreetAddress}</p>
+ <p>${CityStateZipAddress}</p>
+ <p>${PhonyNumb}<p/>
+ <p>"#######"</p>
+ </div>
+ `
+    let rowNumber = 0
+    if (i <= 3) {
+        rowNumber = 1
+    } else if (i > 3 && i <= 7) {
+        rowNumber = 2
+    } else if (i > 7) {
+        rowNumber = 3
+    }
 
-const StreetAddress = cust[0].location.street
-const CityStateZipAddress =`${cust[0].location.city}, ${cust[0].location.state} ${cust[0].location.postcode} `;
-console.log(StreetAddress);
-console.log(CityStateZipAddress);
+    let id = "row" + rowNumber;
 
-const PhonyNumb = cust[0].phone;
-console.log(PhonyNumb);
+
+let ourRow = document.getElementById(id);
+
+let INDIVPerson = document.createElement('div');
+INDIVPerson.className = 'PersonINFO';
+
+  ourRow.appendChild(INDIVPerson);
+
+    // console.log("id is " + id);
+  INDIVPerson.innerHTML = PersonINFO;
+}
+
+
+
+//l
+
+//I need to get this to repeat three times, each time in a new 'section'
